12a3b908fa1386c9bc8233f8f7b0d4b303d7411d
Simon Warta <simon@kullo.net>
2016-05-02 20:22:49 +0200

    Build ICU

--- build.sh
+++ build.sh
@@ -4,14 +4,15 @@ which shellcheck > /dev/null && shellcheck "$0"
 
 QT_VERSION="5.6.0"
 QT_SOURCE="$1"
+CORES=$(nproc)
 
 BUILDDIR="/run/shm/qt-build-$USER"
 INSTALL_ROOT="/opt/qt-$QT_VERSION-linux-clang-libc++"
-INSTALL_SRC="$PREFIX_ROOT/src"
+INSTALL_SRC="$INSTALL_ROOT/src"
+INSTALL_ICU="$INSTALL_ROOT/icu"
 
-export LD_LIBRARY_PATH="/usr/local/lib:."
-
-# sudo sudo apt-get install clang-3.6
+# sudo apt-get install -y libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libgl1-mesa-dev libgtk2.0-dev
+# sudo sudo apt-get install clang-3.6 libc++-dev
 # sudo update-alternatives --install /usr/bin/cc      cc      /usr/bin/clang-3.6 100
 # sudo update-alternatives --install /usr/bin/c++     c++     /usr/bin/clang++-3.6 100
 # sudo update-alternatives --install /usr/bin/clang   clang   /usr/bin/clang-3.6 100
@@ -22,41 +23,38 @@ export LD_LIBRARY_PATH="/usr/local/lib:."
 # sudo update-alternatives --config cc
 # sudo update-alternatives --config c++
 
-sudo apt-get install -y \
-    libfontconfig1-dev \
-    libfreetype6-dev \
-    libx11-dev \
-    libxext-dev \
-    libxfixes-dev \
-    libxi-dev \
-    libxrender-dev \
-    libxcb1-dev \
-    libx11-xcb-dev \
-    libxcb-glx0-dev \
-    libgl1-mesa-dev \
-    libgtk2.0-dev \
-    libc++-dev
-
-if [ ! -w "$PREFIX_ROOT" ] ; then
+if [ ! -w "$INSTALL_ROOT" ] ; then
     echo "Can not write to directory '$PREFIX_ROOT'"
     exit 1
 fi
 
-if ! mkdir -p "$PREFIX_SRC"; then
-    echo "Could not create source directory '$PREFIX_SRC'"
+# Build ICU
+(
+    cd icu/source
+    CXXFLAGS="-stdlib=libc++" ./configure --prefix="$INSTALL_ICU"
+    make -j "$CORES"
+    make install
+)
+
+# Install Qt sources
+if ! mkdir -p "$INSTALL_SRC"; then
+    echo "Could not create source directory '$INSTALL_SRC'"
     exit 1
 fi
-rsync --archive --delete "$QT_SOURCE/" "$PREFIX_SRC"
+rsync --archive --delete "$QT_SOURCE/" "$INSTALL_SRC"
+
+# Build Qt
+export LD_LIBRARY_PATH="$INSTALL_ICU/lib"
 
 for MODE in debug release; do
-    PREFIX="$PREFIX_ROOT/$MODE"
+    PREFIX="$INSTALL_ROOT/$MODE"
 
     rm -rf "$BUILDDIR"
     mkdir -p "$BUILDDIR"
 
     (
         cd "$BUILDDIR"
-        "$PREFIX_SRC/configure" \
+        "$INSTALL_SRC/configure" \
             -opensource \
             -confirm-license \
             -nomake examples \
@@ -69,8 +67,8 @@ for MODE in debug release; do
             -$MODE \
             -qt-xcb \
             -icu \
-            -I /usr/local/include \
-            -L /usr/local/lib \
+            -I "$INSTALL_ICU/include" \
+            -L "$INSTALL_ICU/lib" \
             -gtkstyle \
             -skip qt3d \
             -skip qtactiveqt \
@@ -93,7 +91,7 @@ for MODE in debug release; do
             -skip qtx11extras \
             -skip qtxmlpatterns \
             -shared
-        time CCACHE_DISABLE=1 make -j "$(nproc)"
+        time CCACHE_DISABLE=1 make -j "$CORES"
         make install
     )
 done
