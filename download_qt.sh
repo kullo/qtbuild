69636f89af85605c1241fc83dd1c6beac6a43d72
Simon Warta <simon@kullo.net>
2016-05-03 13:50:56 +0200

    Add Qt downloader

---
+++ download_qt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+which shellcheck > /dev/null && shellcheck "$0"
+
+# http://download.qt.io/official_releases/qt/5.6/5.6.0/single/qt-everywhere-opensource-src-5.6.0.tar.gz.mirrorlist
+URL="http://download.qt.io/official_releases/qt/5.6/5.6.0/single/qt-everywhere-opensource-src-5.6.0.tar.gz"
+LOCAL_FILE="qt-everywhere-opensource-src-5.6.0.tar.gz"
+
+wget -O "$LOCAL_FILE" "$URL"
+sha1sum "$LOCAL_FILE" | grep "4f111a4d6bb90eaed024b857b1bd3d0731ace8a2"
+tar -xf "$LOCAL_FILE"
+rm "$LOCAL_FILE"
