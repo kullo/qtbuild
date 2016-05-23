5a3c0f7791fc29456a2410bf894307ea9b3dda99
Simon Warta <simon@kullo.net>
2016-05-23 20:42:19 +0200

    Fix installer tool

--- install_packages.sh
+++ install_packages.sh
@@ -3,12 +3,10 @@ set -o errexit -o nounset -o pipefail
 which shellcheck > /dev/null && shellcheck "$0"
 
 apt-get install -y \
-    # Handy system tools:
-    joe htop git \
-    # Required by this script:
-    realpath pigz wget \
-    libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libgl1-mesa-dev libgtk2.0-dev \
-    clang-3.6 libc++-dev
+  joe htop git \
+  realpath pigz wget \
+  libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libgl1-mesa-dev libgtk2.0-dev \
+  clang-3.6 libc++-dev
 
 # Setup compiler:
 #   sudo update-alternatives --install /usr/bin/cc      cc      /usr/bin/clang-3.6   100
