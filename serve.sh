fedf68cd06dbfe1982dc3ffd10ea121ceaa0ef9f
Simon Warta <simon@kullo.net>
2016-05-23 20:39:07 +0200

    Add missing suffix .part

--- serve.sh
+++ serve.sh
@@ -14,7 +14,7 @@ if [ "$OS_NAME" = "linux64" ]; then PORT=8064; fi
     for filename in *; do
         URL="http://$(hostname).cloudapp.net:$PORT/$filename"
         HASH=$(sha1sum "$filename" | cut -d" " --fields=1)
-        echo "  wget $URL -O $filename.part && (sha1sum $filename | grep $HASH) && mv $filename.part $filename"
+        echo "  wget $URL -O $filename.part && (sha1sum $filename.part | grep $HASH) && mv $filename.part $filename"
     done
 
     python3 -m http.server "$PORT"
