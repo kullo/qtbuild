50bd35587b1a6583d7aa48caf88bd5268bd4c603
Simon Warta <simon@kullo.net>
2016-07-04 12:49:18 +0200

    Automatically download qt when creating linux32

--- create_linux32_subsystem.sh
+++ create_linux32_subsystem.sh
@@ -41,3 +41,4 @@ schroot -c trusty32 --directory / -- sudo chown kullo:kullo "$WORKSPACE"
 schroot -c trusty32 --directory "$WORKSPACE" -- sudo git clone "https://github.com/webmaster128/qtbuild.git"
 schroot -c trusty32 --directory "$WORKSPACE" -- sudo chown -R kullo:kullo qtbuild
 schroot -c trusty32 --directory "$WORKSPACE/qtbuild" -- sudo ./install_packages.sh
+schroot -c trusty32 --directory "$WORKSPACE/qtbuild" -- sudo ./download_qt.sh
