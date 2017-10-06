89452696ff91388c1ccad2b148a30b674f40f830
Simon Warta <simon@kullo.net>
2017-10-06 14:07:18 +0200

    Update for qt 5.9.2

--- download_qt.sh
+++ download_qt.sh
@@ -4,17 +4,17 @@ which shellcheck > /dev/null && (shellcheck -x "$0" || shellcheck "$0")
 
 source "./_includes.sh"
 
-URL="http://download.qt.io/official_releases/qt/5.9/5.9.1/single/qt-everywhere-opensource-src-5.9.1.tar.xz"
-LOCAL_FILE="qt-everywhere-opensource-src-5.9.1.tar.xz"
-SHA1_HASH="8b9900cece0a18cf23d53a42379a628a1c1330ae"
+URL="http://download.qt.io/official_releases/qt/5.9/5.9.2/single/qt-everywhere-opensource-src-5.9.2.tar.xz"
+LOCAL_FILE="qt-everywhere-opensource-src-5.9.2.tar.xz"
+MD5_HASH="738d1b98106e1bd39f00cc228beb522a"
 
 mkdir -p "$DOWNLOADS_DIR"
 (
     cd "$DOWNLOADS_DIR"
     wget -O "$LOCAL_FILE" "$URL"
-    if [ "$SHA1_HASH" != "" ]; then
+    if [ "$MD5_HASH" != "" ]; then
         echo "Checking checksum ..."
-        sha1sum "$LOCAL_FILE" | fgrep "$SHA1_HASH"
+        md5sum "$LOCAL_FILE" | fgrep "$MD5_HASH"
     fi
     echo "Extracting $LOCAL_FILE ..."
     tar xf "$LOCAL_FILE"
