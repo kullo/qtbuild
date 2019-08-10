85c7d0770d682fa7d8ad4ee527286ee9d5d7ad6c
Simon Warta <simon@warta.it>
2019-08-10 13:15:52 +0200

    Install missing rsync

--- install_packages.sh
+++ install_packages.sh
@@ -7,7 +7,7 @@ apt upgrade -y
 
 apt install -y \
   joe htop git \
-  pigz wget \
+  pigz wget rsync \
   libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libgl1-mesa-dev libgtk-3-dev \
   libasound2-dev libpulse-dev \
   libcups2-dev \
