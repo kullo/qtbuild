d14c93f1ed9c2fb27b397c05cb502929efc4dd75
Simon Warta <simon@warta.it>
2019-08-10 13:03:32 +0200

    Update to Ubuntu 18.04

--- install_packages.sh
+++ install_packages.sh
@@ -2,24 +2,20 @@
 set -o errexit -o nounset -o pipefail
 which shellcheck > /dev/null && (shellcheck -x "$0" || shellcheck "$0")
 
-apt-get update
-apt-get upgrade -y
+apt update
+apt upgrade -y
 
-apt-get install -y \
+apt install -y \
   joe htop git \
-  realpath pigz wget \
+  pigz wget \
   libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libgl1-mesa-dev libgtk-3-dev \
   libasound2-dev libpulse-dev \
   libcups2-dev \
-  clang-3.6 libc++-dev \
+  clang libc++-dev \
   debootstrap schroot
 
-update-alternatives --install /usr/bin/cc      cc      /usr/bin/clang-3.6   100
-update-alternatives --install /usr/bin/c++     c++     /usr/bin/clang++-3.6 100
-update-alternatives --install /usr/bin/clang   clang   /usr/bin/clang-3.6   100
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
+update-alternatives --install /usr/bin/cc      cc      /usr/bin/clang    100
+update-alternatives --install /usr/bin/c++     c++     /usr/bin/clang++  100
 
-update-alternatives --set cc      /usr/bin/clang-3.6
-update-alternatives --set c++     /usr/bin/clang++-3.6
-update-alternatives --set clang   /usr/bin/clang-3.6
-update-alternatives --set clang++ /usr/bin/clang++-3.6
+update-alternatives --set cc      /usr/bin/clang
+update-alternatives --set c++     /usr/bin/clang++
